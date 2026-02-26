cfefe545265a6c7e13ba062959b6a7572b660df7
dorianlorenzato-max <dorian.lorenzato@eiffage.com>
2026-02-26 09:58:53 +0100

    Update api.js

--- api.js
+++ api.js
@@ -286,7 +286,7 @@ async function saveConfigurationFile(triconnectAPI, accessToken, configurationDa
 // Récupération de l'arborescence du projet Trimble
 
 async function fetchFolderContents(folderId, accessToken) {
-  const folderId = "MkvA_YZPfBk";
+  //const folderId = "MkvA_YZPfBk";
   const listItemsUrl = `https://app21.connect.trimble.com/tc/api/2.0/folders/${folderId}/items`;
 
   const response = await fetch(listItemsUrl, {
@@ -312,6 +312,7 @@ async function fetchFolderContents(folderId, accessToken) {
 
 // On exporte la fonction principale pour qu'elle soit utilisable dans main.js
 export { fetchVisaDocuments, fetchProjectGroups, saveConfigurationFile, fetchConfigurationFile, fetchFolderContents };
+
 
 
 
